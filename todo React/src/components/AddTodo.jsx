124e7c2e433bda49c2eecfd62e584bbfda861820
Gauravyb1325 <gauravyb1325@gmail.com>
2024-09-21 18:38:45 +0530

    version 2 Commit

--- todo React/src/components/AddTodo.jsx
+++ todo React/src/components/AddTodo.jsx
@@ -1,11 +1,11 @@
 function AddTodo(){
   return (
 
-    <div class="container text-center">
-      <div class="row g-row">
-      <div class="col-6"><input type="text"             placeholder="Enter Todo Here"/></div>
-      <div class="col-4"><input type="date" /></div>
-      <div class="col-2"><button type="button" class="btn   btn-success g-button">Add</button></div>
+    <div className="container text-center">
+      <div className="row g-row">
+      <div className="col-6"><input type="text"             placeholder="Enter Todo Here"/></div>
+      <div className="col-4"><input type="date" /></div>
+      <div className="col-2"><button type="button" className="btn   btn-success g-button">Add</button></div>
       </div>
     </div>
 
